da892d2145fb5de34795b0ac417c407dbcc5b97d
jj13679 <jpjerosch@gmail.com>
2025-09-25 23:40:05 -0300

    IMplementando o arquivo createtable.php para otimização

---
+++ 2.0/php/createtable.php
@@ -0,0 +1,17 @@
+<?php
+include "conexao.php";
+
+$sql = "
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);";
+if (mysqli_query($conexao, $sql)) {
+    echo "Tabela 'usuarios' criada com sucesso.";
+} else {
+    echo "Erro ao criar tabela: " . mysqli_error($conexao);
+}
+?>
